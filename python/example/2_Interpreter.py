0e149c704513c1e360652ae0d9dbaecc6f8ae679
wonkyuh <wonkyuh@naver.com>
2023-11-10 16:57:39 +0900

    update 1110(2)

---
+++ python/example/2_Interpreter.py
@@ -0,0 +1,18 @@
+## input으로 사용자에게 입력 받기
+# print('직각삼각형 그리기\n')
+# leg=int(input('변의 길이:'))
+
+# for i in range(leg):
+#     print('* ' * (i+1))
+
+# area=(leg**2)/2
+# print('넓이:',area)
+
+
+
+# print('사용자에게 정수를 입력 받아 제곱을 계산하기\n')
+# num=int(input('정수를 입력하시오 : '))
+
+# result=num**2
+# print('결과 : ',result)
+
---
+++ python/example/3_control.py
@@ -0,0 +1,22 @@
+# while을 사용하는 반복문
+# print(1)
+# print(2)
+# print(3)
+
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num = num + 1
+
+# innum=int(input('정수를 입력하시오 : '))
+# i = 1 
+# while i <= innum :
+#     print(innum)
+#     i+=1
+
+# num=int(input('정수를 입력하시오 : '))
+# i=1
+# while i <= num:
+#     print(i,' ',i**2)
+#     i+=1
+
