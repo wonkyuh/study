f840666e7d4e36219e2afefe1cf3dfb76e312790
wonkyuh <wonkyuh@naver.com>
2023-11-10 13:29:06 +0900

    update 1110

---
+++ python/example/hello.py
@@ -0,0 +1,31 @@
+import math
+math.sqrt(550**2+600**2)
+
+watch = 100000
+lightr = 300
+watch+lightr
+
+1000000*0.85
+price=5000
+price*=0.85
+price
+
+a = 'pig'
+b = 'dad'
+a+b
+
+a + ' ' + b
+
+download=800
+time=110
+mb=1000
+
+download*time/mb
+
+family =['mother','father','son','daughter']
+
+family[2]
+family.remove('son')
+family
+
+print('직각삼각형 그리기 \n')
